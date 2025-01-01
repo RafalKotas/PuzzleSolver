31833c8d3dba8a0b29629a8c892a96e1405fb5f7
Raffel222 <Raffel222@gmail.com>
2025-01-01 23:09:20 +0100

    Folder hierarchy change (#8)

---
+++ PuzzleSolver/src/main/java/com/puzzlesolverappbackend/puzzleAppFileManager/interfaces/NonogramLogicPrinter.java
@@ -0,0 +1,16 @@
+package com.puzzlesolverappbackend.puzzleAppFileManager.interfaces;
+
+public interface NonogramLogicPrinter {
+
+    void printNonogramBoard();
+
+    void printNonogramBoardWithMarks();
+
+    void printRowsSequencesRanges();
+
+    void printColumnsSequencesRanges();
+
+    void printLogs();
+
+    void printStats();
+}
