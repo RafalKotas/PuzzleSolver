79e8fc2ab0ee8091d6d9065923116b755ce8c204
Raffel222 <Raffel222@gmail.com>
2025-01-06 19:13:40 +0100

    Vaildate column coloured sequence

--- PuzzleSolver/src/main/java/com/puzzlesolverappbackend/puzzleAppFileManager/utils/NonogramLogicUtils.java
+++ PuzzleSolver/src/main/java/com/puzzlesolverappbackend/puzzleAppFileManager/utils/NonogramLogicUtils.java
@@ -1,5 +1,6 @@
 package com.puzzlesolverappbackend.puzzleAppFileManager.utils;
 
+import com.puzzlesolverappbackend.puzzleAppFileManager.puzzlespecific.nonogram.logic.NonogramColumnLogic;
 import com.puzzlesolverappbackend.puzzleAppFileManager.puzzlespecific.nonogram.logic.NonogramRowLogic;
 import lombok.experimental.UtilityClass;
 
@@ -16,13 +17,24 @@ public class NonogramLogicUtils {
         List<Integer> rowSequencesLengths = nonogramRowLogic.getRowsSequences().get(rowIdx);
         List<List<Integer>> rowSequencesRanges = nonogramRowLogic.getRowsSequencesRanges().get(rowIdx);
 
-        if (rowIdx == 10 && nonogramRowLogic.getNonogramSolutionBoard().get(10).get(6).equals("O")) {
-            System.out.println("abc");
-        }
-
         for(int seqNo = 0; seqNo < rowSequencesLengths.size(); seqNo++) {
             if (rangeInsideAnotherRange(colouredSequence, rowSequencesRanges.get(seqNo)) &&
                     rangeLength(colouredSequence) <= rowSequencesLengths.get(seqNo)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean colouredSequenceInColumnIsValid(List<Integer> colouredSequence, int columnIdx,
+                                                       NonogramColumnLogic nonogramColumnLogic) {
+        List<Integer> columnSequencesLengths = nonogramColumnLogic.getColumnsSequences().get(columnIdx);
+        List<List<Integer>> columnSequencesRanges = nonogramColumnLogic.getColumnsSequencesRanges().get(columnIdx);
+
+        for(int seqNo = 0; seqNo < columnSequencesLengths.size(); seqNo++) {
+            if (rangeInsideAnotherRange(colouredSequence, columnSequencesRanges.get(seqNo)) &&
+                    rangeLength(colouredSequence) <= columnSequencesLengths.get(seqNo)) {
                 return true;
             }
         }
