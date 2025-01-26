4c291191c082cd359e7f4f504cdea933084c5872
Raffel222 <Raffel222@gmail.com>
2025-01-26 21:35:02 +0100

    actionsToDoAfterPlacingXsAroundLongestSequences - add dependencies

--- PuzzleSolver/src/main/java/com/puzzlesolverappbackend/puzzleAppFileManager/constants/ActionsConstants.java
+++ PuzzleSolver/src/main/java/com/puzzlesolverappbackend/puzzleAppFileManager/constants/ActionsConstants.java
@@ -168,6 +168,8 @@ public class ActionsConstants {
     public final static ArrayList<NonogramSolveAction> actionsToDoInColumnAfterPlacingXsAroundLongestSequencesInRows = new ArrayList<>(
             List.of(
                     CORRECT_COLUMN_SEQUENCES_RANGES_IF_X_ON_WAY,
+                    CORRECT_COLUMN_SEQUENCES_RANGES_WHEN_MATCHING_FIELDS_TO_SEQUENCES,
+                    EXTEND_COLOURED_FIELDS_NEAR_X_IN_COLUMN,
                     PLACE_XS_COLUMN_AT_TOO_SHORT_EMPTY_SEQUENCES,
                     PLACE_XS_COLUMN_IF_O_NEAR_X_WILL_BEGIN_TOO_LONG_POSSIBLE_COLOURED_SEQUENCE
             ));
@@ -182,6 +184,8 @@ public class ActionsConstants {
     public final static ArrayList<NonogramSolveAction> actionsToDoInRowAfterPlacingXsAroundLongestSequencesInColumns = new ArrayList<>(
             List.of(
                     CORRECT_ROW_SEQUENCES_RANGES_IF_X_ON_WAY,
+                    CORRECT_ROW_SEQUENCES_RANGES_WHEN_MATCHING_FIELDS_TO_SEQUENCES,
+                    EXTEND_COLOURED_FIELDS_NEAR_X_IN_ROW,
                     PLACE_XS_ROW_AT_TOO_SHORT_EMPTY_SEQUENCES,
                     PLACE_XS_ROW_IF_O_NEAR_X_WILL_BEGIN_TOO_LONG_POSSIBLE_COLOURED_SEQUENCE
             ));
