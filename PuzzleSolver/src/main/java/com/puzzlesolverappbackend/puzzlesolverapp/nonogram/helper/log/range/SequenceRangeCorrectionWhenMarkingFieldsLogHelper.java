154762928952570c532da1df5dd79f30ce26a1fe
Raffel222 <Raffel222@gmail.com>
2025-08-21 19:41:01 +0200

    BE - SequenceRangeCorrectionWhenMarkingFieldsLogHelper - refactor & tests

--- PuzzleSolver/src/main/java/com/puzzlesolverappbackend/puzzlesolverapp/nonogram/helper/log/range/SequenceRangeCorrectionWhenMarkingFieldsLogHelper.java
+++ PuzzleSolver/src/main/java/com/puzzlesolverappbackend/puzzlesolverapp/nonogram/helper/log/range/SequenceRangeCorrectionWhenMarkingFieldsLogHelper.java
@@ -5,8 +5,7 @@ import lombok.experimental.UtilityClass;
 import java.util.List;
 
 import static com.puzzlesolverappbackend.puzzlesolverapp.nonogram.helper.common.HelpersConstants.*;
-import static com.puzzlesolverappbackend.puzzlesolverapp.nonogram.helper.log.loggeneration.LogFormatUtils.toImmutableIntListLiteral;
-import static com.puzzlesolverappbackend.puzzlesolverapp.nonogram.helper.log.loggeneration.LogFormatUtils.toMutableRangesListLiteral;
+import static com.puzzlesolverappbackend.puzzlesolverapp.nonogram.helper.log.loggeneration.LogFormatUtils.*;
 
 @UtilityClass
 public class SequenceRangeCorrectionWhenMarkingFieldsLogHelper {
@@ -14,23 +13,26 @@ public class SequenceRangeCorrectionWhenMarkingFieldsLogHelper {
     public static String generateLog(
             boolean isRow,
             int index,
+            int sequenceIndex,
             List<List<Integer>> sequencesRanges,
-            List<Integer> updatedRange,
-            List<Integer> sequencesLengths
+            List<Integer> sequencesLengths,
+            List<Integer> updatedRange
     ) {
         return String.format(
                 """
                         SEQUENCE_RANGE_CORRECTION_WHEN_MARKING_FIELDS_IN_%s: %s=%d
+                        sequenceIndex=%s
                         sequencesRanges=%s
-                        updatedRange=%s
                         sequencesLengths=%s
+                        updatedRange=%s
                         """,
                 isRow ? ROW_ACTION_NAME : COLUMN_ACTION_NAME,
                 isRow ? ROW : COLUMN,
                 index,
+                sequenceIndex,
                 sequencesRanges.toString(),
-                updatedRange.toString(),
-                sequencesLengths.toString()
+                sequencesLengths.toString(),
+                updatedRange.toString()
         );
     }
 
@@ -40,25 +42,30 @@ public class SequenceRangeCorrectionWhenMarkingFieldsLogHelper {
         boolean isRow = lines[0].contains(ROW_ACTION_NAME);
         String axisLabel = isRow ? ROW : COLUMN;
 
-        int index = Integer.parseInt(lines[0].split(axisLabel + "=")[1].trim());
+        String fileName = solutionName.replaceFirst("^r", "").replaceFirst("\\.json$", "");
 
-        String sequencesRanges = lines[1].contains("=") ? lines[1].split("=")[1].trim() : "";
-        String updatedRange = lines[2].contains("=") ? lines[2].split("=")[1].trim() : "";
-        String sequencesLengths = lines[3].contains("=") ? lines[3].split("=")[1].trim() : "";
+        int index = Integer.parseInt(lines[0].split(axisLabel + "=")[1].trim());
+        int sequenceIndex = Integer.parseInt(lines[1].split("=")[1].trim());
+
+        String sequencesRanges = extractValue(lines, "sequencesRanges");
+        String sequencesLengths = extractValue(lines, "sequencesLengths");
+        String updatedRange = extractValue(lines, "updatedRange");
 
         return String.format(
                 """
                         Arguments.of("%s / %s=%d - sequences range correction when marking fields",
+                            %d,
                             %s,
                             %s,
                             %s
                         )""",
-                solutionName,
+                fileName,
                 isRow ? ROW : COLUMN,
                 index,
+                sequenceIndex,
                 toMutableRangesListLiteral(sequencesRanges),
-                toImmutableIntListLiteral(updatedRange),
-                toImmutableIntListLiteral(sequencesLengths)
+                toImmutableIntListLiteral(sequencesLengths),
+                toMutableIntListLiteral(updatedRange)
         );
     }
 }
--- PuzzleSolver/src/test/java/com/puzzlesolverappbackend/puzzlesolverapp/nonogram/helper/log/range/SequenceRangeCorrectionWhenMarkingFieldsLogHelperTest.java
+++ PuzzleSolver/src/test/java/com/puzzlesolverappbackend/puzzlesolverapp/nonogram/helper/log/range/SequenceRangeCorrectionWhenMarkingFieldsLogHelperTest.java
@@ -1,11 +1,16 @@
 package com.puzzlesolverappbackend.puzzlesolverapp.nonogram.helper.log.range;
 
+import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SequenceRangeCorrectionWhenMarkingFieldsLogHelperTest {
@@ -24,5 +29,143 @@ class SequenceRangeCorrectionWhenMarkingFieldsLogHelperTest {
         Throwable cause = exception.getCause();
         assertInstanceOf(UnsupportedOperationException.class, cause);
         assertEquals("This is a utility class and cannot be instantiated", cause.getMessage());
+    }
+
+    @Test
+    @DisplayName("Generate example log - o08007 row 9")
+    void shouldGenerateLogRowCase() {
+        // given
+        boolean isRow = true;
+        int index = 9;
+        int sequenceIndex = 0;
+
+        List<List<Integer>> sequencesRanges = new ArrayList<>(
+                List.of(
+                        new ArrayList<>(Arrays.asList(0, 14))
+                )
+        );
+        List<Integer> updatedRange = new ArrayList<>(
+                List.of(0, 10)
+        );
+        List<Integer> sequencesLengths = List.of(9);
+
+        // when
+        String log = SequenceRangeCorrectionWhenMarkingFieldsLogHelper.generateLog(isRow,
+                index,
+                sequenceIndex,
+                sequencesRanges,
+                sequencesLengths,
+                updatedRange);
+
+        // then
+        String expected =
+                """
+                        SEQUENCE_RANGE_CORRECTION_WHEN_MARKING_FIELDS_IN_ROW: row=9
+                        sequenceIndex=0
+                        sequencesRanges=[[0, 14]]
+                        sequencesLengths=[9]
+                        updatedRange=[0, 10]
+                        """;
+        assertThat(log).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("Convert example log to test arguments - o08007 row 9")
+    void shouldConvertGeneratedLogToTestArgumentsRowCase() {
+        // given
+        String generatedLog =
+                """
+                        SEQUENCE_RANGE_CORRECTION_WHEN_MARKING_FIELDS_IN_ROW: row=9
+                        sequenceIndex=0
+                        sequencesRanges=[[0, 14]]
+                        sequencesLengths=[9]
+                        updatedRange=[0, 10]
+                        """;
+
+        // when
+        String convertedLog = SequenceRangeCorrectionWhenMarkingFieldsLogHelper.convertLogToTestArguments(
+                generatedLog,
+                "ro08007"
+        );
+
+        // then
+        String expected = """
+                Arguments.of("o08007 / row=9 - sequences range correction when marking fields",
+                    0,
+                    new ArrayList<>(List.of(new ArrayList<>(List.of(0, 14)))),
+                    List.of(9),
+                    new ArrayList<>(List.of(0, 10))
+                )""";
+        AssertionsForClassTypes.assertThat(convertedLog).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("Generate example log - o08007 column 2")
+    void shouldGenerateLogColumnCase() {
+        // given
+        boolean isRow = false;
+        int index = 2;
+        int sequenceIndex = 1;
+
+        List<List<Integer>> sequencesRanges = new ArrayList<>(
+                List.of(
+                        new ArrayList<>(Arrays.asList(1, 4)),
+                        new ArrayList<>(Arrays.asList(6, 11)),
+                        new ArrayList<>(Arrays.asList(13, 14))
+                )
+        );
+        List<Integer> updatedRange = new ArrayList<>(
+                List.of(6, 10)
+        );
+        List<Integer> sequencesLengths = List.of(4, 5, 2);
+
+        // when
+        String log = SequenceRangeCorrectionWhenMarkingFieldsLogHelper.generateLog(isRow,
+                index,
+                sequenceIndex,
+                sequencesRanges,
+                sequencesLengths,
+                updatedRange);
+
+        // then
+        String expected =
+                """
+                        SEQUENCE_RANGE_CORRECTION_WHEN_MARKING_FIELDS_IN_COLUMN: column=2
+                        sequenceIndex=1
+                        sequencesRanges=[[1, 4], [6, 11], [13, 14]]
+                        sequencesLengths=[4, 5, 2]
+                        updatedRange=[6, 10]
+                        """;
+        assertThat(log).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("Convert example log to test arguments - o08007  column 2")
+    void shouldConvertGeneratedLogToTestArgumentsColumnCase() {
+        // given
+        String generatedLog =
+                """
+                        SEQUENCE_RANGE_CORRECTION_WHEN_MARKING_FIELDS_IN_COLUMN: column=2
+                        sequenceIndex=1
+                        sequencesRanges=[[1, 4], [6, 11], [13, 14]]
+                        sequencesLengths=[4, 5, 2]
+                        updatedRange=[6, 10]
+                        """;
+
+        // when
+        String convertedLog = SequenceRangeCorrectionWhenMarkingFieldsLogHelper.convertLogToTestArguments(
+                generatedLog,
+                "ro08007"
+        );
+
+        // then
+        String expected = """
+                Arguments.of("o08007 / column=2 - sequences range correction when marking fields",
+                    1,
+                    new ArrayList<>(List.of(new ArrayList<>(List.of(1, 4)), new ArrayList<>(List.of(6, 11)), new ArrayList<>(List.of(13, 14)))),
+                    List.of(4, 5, 2),
+                    new ArrayList<>(List.of(6, 10))
+                )""";
+        AssertionsForClassTypes.assertThat(convertedLog).isEqualTo(expected);
     }
 }
