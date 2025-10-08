1492f332e14700ffeee13b80b83aedfb7b196ed9
Raffel222 <Raffel222@gmail.com>
2025-10-08 18:14:47 +0200

    BE - NonogramParametersComparatorHelper - tests

--- PuzzleSolver/src/test/java/com/puzzlesolverappbackend/puzzlesolverapp/nonogram/core/util/NonogramParametersComparatorHelperTest.java
+++ PuzzleSolver/src/test/java/com/puzzlesolverappbackend/puzzlesolverapp/nonogram/core/util/NonogramParametersComparatorHelperTest.java
@@ -5,7 +5,13 @@ import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+import static com.puzzlesolverappbackend.puzzlesolverapp.nonogram.core.util.NonogramParametersComparatorHelper.rangesNotEqual;
+import static com.puzzlesolverappbackend.puzzlesolverapp.nonogram.core.util.NonogramParametersComparatorHelper.sequencesRangesEqual;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -24,5 +30,58 @@ class NonogramParametersComparatorHelperTest {
         // then
         Throwable cause = exception.getCause();
         assertInstanceOf(UnsupportedOperationException.class, cause);
+    }
+
+    @Test
+    @DisplayName("Should return false if first sequences ranges size is not equal to second sequences ranges size - o06005 row 0")
+    void shouldReturnFalseSequencesRangesArraysSizeNotEquals() {
+        // given
+        List<List<Integer>> firstSequencesRanges = new ArrayList<>(
+                Arrays.asList(
+                        new ArrayList<>(Arrays.asList(0, 5)),
+                        new ArrayList<>(Arrays.asList(2, 7)),
+                        new ArrayList<>(Arrays.asList(4, 9))
+                )
+        );
+        // empty row checking -> (-1, -1) range
+        List<List<Integer>> secondSequencesRanges = new ArrayList<>(
+                Arrays.asList(
+                        new ArrayList<>(Arrays.asList(-1, -1))
+                )
+        );
+
+        // when
+        boolean areSequencesRangesEqual = sequencesRangesEqual(firstSequencesRanges, secondSequencesRanges);
+
+        // then
+        assertThat(areSequencesRangesEqual).isFalse();
+    }
+
+    @Test
+    @DisplayName("Should return true if first sequence range is not equal to second sequence range - o06005 row 3")
+    void shouldReturnTrueIfFirstSequenceRangeIsNotEqualToSecondSequenceRange() {
+        // given
+        List<Integer> firstRange = new ArrayList<>(List.of(0, 9));
+        List<Integer> secondRange = new ArrayList<>(List.of(-1, -1));
+
+        // when
+        boolean rangesNotEqual = rangesNotEqual(firstRange, secondRange);
+
+        // then
+        assertThat(rangesNotEqual).isTrue();
+    }
+
+    @Test
+    @DisplayName("Should return false if first sequence range is equal to second sequence range - o06005 column 3")
+    void shouldReturnFalseIfFirstSequenceRangeIsNotEqualToSecondSequenceRange() {
+        // given
+        List<Integer> firstRange = new ArrayList<>(List.of(2, 7));
+        List<Integer> secondRange = new ArrayList<>(List.of(2, 7));
+
+        // when
+        boolean rangesNotEqual = rangesNotEqual(firstRange, secondRange);
+
+        // then
+        assertThat(rangesNotEqual).isFalse();
     }
 }
