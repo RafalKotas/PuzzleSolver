2faff41a2fa356bc95d84e264ed63a5bc0a5e548
Raffel222 <Raffel222@gmail.com>
2025-11-01 18:56:55 +0100

    BE - Remove TODO

--- PuzzleSolver/src/main/java/com/puzzlesolverappbackend/puzzlesolverapp/nonogram/service/specification/NonogramSpecification.java
+++ PuzzleSolver/src/main/java/com/puzzlesolverappbackend/puzzlesolverapp/nonogram/service/specification/NonogramSpecification.java
@@ -11,7 +11,6 @@ import org.springframework.data.jpa.domain.Specification;
 import java.util.ArrayList;
 import java.util.List;
 
-// TODO - removee class(?)
 @UtilityClass
 public class NonogramSpecification {
 
