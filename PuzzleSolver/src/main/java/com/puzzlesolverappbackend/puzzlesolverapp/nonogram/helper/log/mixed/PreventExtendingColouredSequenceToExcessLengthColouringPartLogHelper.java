f939137fde7db9d666e9223ae84ba993cfc05f41
Raffel222 <Raffel222@gmail.com>
2025-11-02 16:57:09 +0100

    BE - PreventExtendingColouredSequenceToExcessLengthColouringPartLogHelper - add direction to log

--- PuzzleSolver/src/main/java/com/puzzlesolverappbackend/puzzlesolverapp/nonogram/helper/log/mixed/PreventExtendingColouredSequenceToExcessLengthColouringPartLogHelper.java
+++ PuzzleSolver/src/main/java/com/puzzlesolverappbackend/puzzlesolverapp/nonogram/helper/log/mixed/PreventExtendingColouredSequenceToExcessLengthColouringPartLogHelper.java
@@ -5,6 +5,7 @@ import com.puzzlesolverappbackend.puzzlesolverapp.nonogram.helper.log.colouring.
 import lombok.experimental.UtilityClass;
 
 import static com.puzzlesolverappbackend.puzzlesolverapp.nonogram.helper.common.HelpersConstants.*;
+import static com.puzzlesolverappbackend.puzzlesolverapp.nonogram.helper.log.loggeneration.LogFormatUtils.extractValue;
 
 @UtilityClass
 public class PreventExtendingColouredSequenceToExcessLengthColouringPartLogHelper {
@@ -36,10 +37,13 @@ public class PreventExtendingColouredSequenceToExcessLengthColouringPartLogHelpe
     // TODO - include direction in log
     public static String convertLogToTestArguments(String log, String solutionName) {
         ColouringConvertLogBaseContext colouringConvertLogBaseContext = new ColouringConvertLogBaseContext(log, solutionName);
+        String[] lines = log.split("\\n");
+        String direction = extractValue(lines, "direction");
 
         return String.format(
                 """
                         Arguments.of("%s / %s=%d - prevent extending coloured sequence to excess length colouring part",
+                            %s,
                             %s,
                             %s,
                             %s,
@@ -48,6 +52,7 @@ public class PreventExtendingColouredSequenceToExcessLengthColouringPartLogHelpe
                 colouringConvertLogBaseContext.getFileName(),
                 colouringConvertLogBaseContext.getAxisLabel(),
                 colouringConvertLogBaseContext.getIndex(),
+                direction,
                 colouringConvertLogBaseContext.getInitialLine(),
                 colouringConvertLogBaseContext.getSequencesRanges(),
                 colouringConvertLogBaseContext.getSequencesLengths(),
