f8b5fac130a95532e7a3f63c295366bb99761fe1
Raffel222 <Raffel222@gmail.com>
2025-11-09 19:58:54 +0100

    BE - tests - add DisplayName and refactor 2

--- PuzzleSolver/src/test/java/com/puzzlesolverappbackend/puzzlesolverapp/common/vo/PublicationTest.java
+++ PuzzleSolver/src/test/java/com/puzzlesolverappbackend/puzzlesolverapp/common/vo/PublicationTest.java
@@ -1,5 +1,6 @@
 package com.puzzlesolverappbackend.puzzlesolverapp.common.vo;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
